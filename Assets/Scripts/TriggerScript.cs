dcbd05196003038859b55e1e1f6906faba7b1b86
Jordan Davis <jordandavisporter@Hotmail.com>
2021-12-04 13:10:34 -0400

    Added more atmosphere and auto quit function after timeline finishes

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerScript : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+
+        }
+    }
+
+
+
+
+}
